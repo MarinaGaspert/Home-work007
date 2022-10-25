5d1f62b6a3605d713746e3e0da6028c5443b8b48
Marina <gaspertrishka@mail.ru>
2022-10-25 20:36:16 +0700

    задание 1

---
+++ home work01/Program.cs
@@ -0,0 +1,42 @@
+﻿ /*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] resultMatrix= new int[m,n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultMatrix[i,j] = rnd.Next(-10,10);
+        }
+    }
+    return resultMatrix;
+}
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitMatrix(m,n);
+PrintMatrix(matrix);
